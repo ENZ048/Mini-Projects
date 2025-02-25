0d2e4efa915cee6211d83888da02ac12d1806926
Pratik Yesare <pratik.yesare68@gmail.com>
2025-02-25 23:58:10 +0530

    Added Styling

--- loan-calculator/src/LoanDashBoard.jsx
+++ loan-calculator/src/LoanDashBoard.jsx
@@ -40,13 +40,13 @@ export default function LoanDashBoard() {
 
   return (
     <div className="p-8 bg-gray-100 min-h-screen flex flex-col items-center">
-        <h1 className="text-center text-4xl">Loan Calculator</h1>
+        <h1 className="text-center text-4xl">EMI Calculator</h1>
 
-        <div className="container width[80vw] flex gap-4 justify-center items-center">
-            <div className="input-container">
-                <div className="input-element">
-                    <h3>Principle</h3>
-                    <h1>₹ {principle}</h1>
+        <div className="container w[80vw] flex gap-4 justify-between items-center mt-8">
+            <div className="input-container w-1/2">
+                <div className="input-element w-full">
+                    <h3 className="mb-2">Principle</h3>
+                    <h1 className="text-2xl">₹ {principle}</h1>
                     <input 
                         type="range"
                         min="100000"
@@ -58,65 +58,69 @@ export default function LoanDashBoard() {
                             setPrinciple(newValue);
                             setLoanAmount(newValue - downPayment);
                         }}
-                         
+                         className="w-full"
                     />
                 </div>
 
-                <div className="input-element">
-                    <h3>DownPayment</h3>
-                    <h1>₹ {downPayment}</h1>
+                <div className="input-element w-full">
+                    <h3 className="mb-2">DownPayment</h3>
+                    <h1 className="text-2xl">₹ {downPayment}</h1>
                     <input 
                         type="range"
                         min="0"
                         max={principle}
                         step="50000"
                         value={downPayment}
-                        onChange={(e) => changeDownPayment(Number(e.target.value))}  
+                        onChange={(e) => changeDownPayment(Number(e.target.value))}
+                        className="w-full"      
                     />
                 </div>
 
-                <div className="input-element">
-                    <h3>Loan Amount</h3>
-                    <h1>₹ {loanAmount}</h1>
+                <div className="input-element w-full">
+                    <h3 className="mb-2">Loan Amount</h3>
+                    <h1 className="text-2xl">₹ {loanAmount}</h1>
                     <input 
                         type="range"
                         min="0"
                         max={principle}
                         step="50000"
                         value={loanAmount}
-                        onChange={(e) => changeLoanAmount(Number(e.target.value))}  
+                        onChange={(e) => changeLoanAmount(Number(e.target.value))}
+                        className="w-full"      
                     />
                 </div>
 
-                <div className="input-element">
-                    <h3>Tenure (Years)</h3>
-                    <h1>{tenure} years</h1>
+                <div className="input-element w-full">
+                    <h3 className="mb-2">Tenure (Years)</h3>
+                    <h1 className="text-2xl">{tenure} years</h1>
                     <input 
                         type="range"
                         min="0"
                         max="30"
                         step="1"
                         value={tenure}
-                        onChange={(e) => setTenure(Number(e.target.value))}  
+                        onChange={(e) => setTenure(Number(e.target.value))}
+                        className="w-full"      
                     />
                 </div>
 
-                <div className="input-element">
-                    <h3>Interest Rate</h3>
-                    <h1>{interesetRate}%</h1>
+                <div className="input-element w-full">
+                    <h3 className="mb-2">Interest Rate</h3>
+                    <h1 className="text-2xl">{interesetRate}%</h1>
                     <input 
                         type="range"
                         min="0"
                         max="20"
                         step="0.1"
                         value={interesetRate}
-                        onChange={(e) => setInteresetRate(Number(e.target.value))}  
+                        onChange={(e) => setInteresetRate(Number(e.target.value))}
+                        className="w-full"      
                     />
                 </div>
 
 
             </div>
-            <div className="output-container flex flex-col items-center">
+            <div className="output-container flex flex-col items-center w-1/3 text-2xl md:text-lg">
                 <h1>Monthly Emi : <strong>₹ {monthlyEMI.toFixed(2)}</strong></h1>
                 <h1>Total Interest : <strong>₹ {totalInterest.toFixed(2)}</strong></h1>
                 <Pie data={chartData}/>
